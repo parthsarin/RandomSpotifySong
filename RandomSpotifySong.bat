760544393891ce7b43cd6c6b3b63f5e18c9bb13a
Joe Dickson <joedickson4556@gmail.com>
2021-02-21 15:50:49 +1000

    stopped batch script from producing output and made it work without requirements.txt

--- RandomSpotifySong.bat
+++ RandomSpotifySong.bat
@@ -1,8 +1,1 @@
-@echo off
-python -m venv .env
-cd .env/Scripts 
-activate 
-cd .. 
-cd .. 
-pip install -r requirements.txt 
-python randomsong.py
+@echo off && echo Loading... && python -m venv .env && cd .env/Scripts && activate && cd .. && cd .. && pip install spotipy --quiet --quiet && python randomsong.py
